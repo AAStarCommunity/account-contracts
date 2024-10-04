d08096fc332b3c6c16debe88a5109433a82d7667
0xcy <993921@qq.com>
2024-10-04 21:47:16 +0800

    bls sig-verify

--- src/BLSLightAccount.sol
+++ src/BLSLightAccount.sol
@@ -1,27 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.23;
 
-import {BLSOpen} from  "./BLSOpen.sol";
+import {BLSOpen} from "../lib/BLSOpen.sol";
 
 contract BLSLightAccount {
-
     uint256[4] private publicKey;
     address private owner;
-    
-    function validateUserOpSignature(uint256[] message, uint256[2] signature)
-    external view returns (bool)  {
+
+    constructor(address owner_) {
+        if (owner_ == address(0)) {
+            revert("invalid owner");
+        }
+        owner = owner_;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+
+    function validateUserOpSignature(uint256[2] memory message, uint256[2] memory signature)
+        external
+        view
+        returns (bool)
+    {
         return BLSOpen.verifySingle(signature, publicKey, message);
     }
 
-    function initialize(address owner_, uint256[4] memory aPublicKey) external {
-        if (owner_ == address(0)) {
-            revert InvalidOwner(address(0));
-        }
+    function initialize(address owner_, uint256[4] memory aPublicKey) external onlyOwner {
         owner = owner_;
         publicKey = aPublicKey;
     }
 
-    function setBlsPublicKey(uint256[4] memory newPublicKey) public {
+    function setBlsPublicKey(uint256[4] memory newPublicKey) public onlyOwner {
         publicKey = newPublicKey;
     }
 }
